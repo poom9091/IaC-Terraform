eed94075256c68196ab9ab4b84cfef4135ee5564
Pubadee Klinocha <poom90914@gmail.com>
2021-10-22 03:40:32 +0700

    Change S3 bucket prefix

--- modules/pipeline-S3-Host/s3.tf
+++ modules/pipeline-S3-Host/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket        = lower(var.CODEPIPELINE_NAME)
+  bucket        = "${lower(var.PROJECT_NAME)}-artifact/${lower(var.ENV)}-pipeline"
   acl           = "private"
   force_destroy = true
 }
